fc11a9fe00ac0d8ef5522a310745249bc99a4584
Muhammad Islam <mislam0525@gamil.com>
2023-10-22 12:53:15 -0400

    Dequeue

--- 44-Queue/Queue.py
+++ 44-Queue/Queue.py
@@ -23,13 +23,27 @@ class Queue:
         new_node = Node(value)
         if self.length == 0:
             self.first = new_node
-            self.last = new_node
-            self.length = 1
-            return True
-        self.last.next = new_node
+        else:
+            self.last.next = new_node
+
         self.last = new_node
         self.length += 1
         return True
+
+    def dequeue(self):
+        if self.first is None:
+            return None
+        
+        temp = self.first
+        if self.length == 1:
+            self.first = None
+            self.last = None
+        else:
+            self.first = self.first.next
+            temp.next = None
+        self.length -= 1
+
+        return temp
 
 my_q = Queue(4)
 my_q.print_quqeue()
@@ -38,3 +52,7 @@ print("Enqueue")
 my_q.enqueue(5)
 my_q.enqueue(6)
 my_q.print_quqeue()
+
+print("Dequeue: ", my_q.dequeue().value)
+my_q.print_quqeue()
+

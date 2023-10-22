6d430443adc1a6d481fd9b08342f33a1acb647c3
Muhammad Islam <mislam0525@gamil.com>
2023-10-22 14:12:52 -0400

    Balanced Parentheses done

---
+++ 33-stack/StackList.py
@@ -0,0 +1,24 @@
+
+class Stack:
+    def __init__(self):
+        self.stack_list = []
+        
+    def print_stack(self):
+        for i in range(len(self.stack_list)-1, -1, -1):
+            print(self.stack_list[i])
+
+    def push(self, value):
+        self.stack_list.append(value)
+        return True
+
+    def pop(self) -> None:
+        if len(self.stack_list) == 0:
+            return None
+        return self.stack_list.pop()
+
+my_stack = Stack()
+my_stack.push(1)
+my_stack.push(2)
+my_stack.push(3)
+
+my_stack.print_stack()

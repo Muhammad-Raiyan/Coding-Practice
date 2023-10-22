03c9e4035bebb4ce44bbeab6bd4e26a05402d91b
Muhammad Islam <mislam0525@gamil.com>
2023-10-22 17:41:04 -0400

    Sort Stack Completed

---
+++ 33-stack/SortStack.py
@@ -0,0 +1,81 @@
+class Stack:
+    def __init__(self):
+        self.stack_list = []
+
+    def print_stack(self):
+        for i in range(len(self.stack_list)-1, -1, -1):
+            print(self.stack_list[i], end=" ")
+        print()
+
+    def is_empty(self):
+        return len(self.stack_list) == 0
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        else:
+            return self.stack_list[-1]
+
+    def size(self):
+        return len(self.stack_list)
+
+    def push(self, value):
+        self.stack_list.append(value)
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        else:
+            return self.stack_list.pop()
+
+def sort_stack(input_stack):
+    if input_stack.size() <= 1:
+        return 
+    sorted_stack = Stack()
+    sorted_stack.push(input_stack.pop())
+
+
+    temp = input_stack.pop()
+    while temp is not None:
+        if not sorted_stack.is_empty() and sorted_stack.peek() < temp:
+            input_stack.push(sorted_stack.pop())
+        else:
+            sorted_stack.push(temp)
+            temp = input_stack.pop()
+    input_stack.stack_list = sorted_stack.stack_list
+    
+my_stack = Stack()
+my_stack.push(3)
+my_stack.push(1)
+my_stack.push(5)
+my_stack.push(4)
+my_stack.push(2)
+
+print("Stack before sort_stack():")
+my_stack.print_stack()
+
+sort_stack(my_stack)
+
+print("\nStack after sort_stack:")
+my_stack.print_stack()
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Stack before sort_stack():
+    2
+    4
+    5
+    1
+    3
+
+    Stack after sort_stack:
+    1
+    2
+    3
+    4
+    5
+
+"""

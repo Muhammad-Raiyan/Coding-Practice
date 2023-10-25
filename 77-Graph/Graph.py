160c96a428dffb99c62b8ea5904eb43e58df2187
Muhammad Islam <mislam0525@gamil.com>
2023-10-25 15:10:44 -0400

    Graph add_edge

--- 77-Graph/Graph.py
+++ 77-Graph/Graph.py
@@ -12,10 +12,19 @@ class Graph:
             return True
         return False
 
+    def add_edge(self, v1, v2):
+        if v1 not in self.adj_list or v2 not in self.adj_list:
+            return False
+        self.adj_list[v1].append(v2)
+        self.adj_list[v2].append(v1)
+
 
 my_graph = Graph()
 
 my_graph.add_vertex(5)
-my_graph.add_vertex(5)
+my_graph.add_vertex(2)
 
+my_graph.print_graph()
+
+my_graph.add_edge(2, 5)
 my_graph.print_graph()

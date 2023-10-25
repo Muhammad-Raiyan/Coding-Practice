04e06dfae6213a4ea2657cc5459fa9ecb059d857
Muhammad Islam <mislam0525@gamil.com>
2023-10-25 15:26:36 -0400

    Remove edge

--- 77-Graph/Graph.py
+++ 77-Graph/Graph.py
@@ -18,13 +18,30 @@ class Graph:
         self.adj_list[v1].append(v2)
         self.adj_list[v2].append(v1)
 
+    def remove_edge(self, v1, v2):
+        if v1 not in self.adj_list or v2 not in self.adj_list:
+            return False
+        if v2 in self.adj_list[v1] and v1 in self.adj_list[v2]:
+            self.adj_list[v1].remove(v2)
+            self.adj_list[v2].remove(v1)
+
+        return True
+
 
 my_graph = Graph()
 
 my_graph.add_vertex(5)
 my_graph.add_vertex(2)
+my_graph.add_vertex(1)
 
 my_graph.print_graph()
 
 my_graph.add_edge(2, 5)
+
+my_graph.add_edge(2, 1)
+my_graph.print_graph()
+
+my_graph.remove_edge(1, 2)
+
+my_graph.remove_edge(1, 2)
 my_graph.print_graph()

c2d7e2e1141f19a3e655e273de957ca7af9e094f
Raiyan <mislam0525@gmail.com>
2023-10-25 20:20:33 -0400

    Heap helper methods

---
+++ 88-Heap/Heap.py
@@ -0,0 +1,9 @@
+class MaxHeap:
+    def __init__(self) -> None:
+        self.heap = []
+
+    def _left_child(self, index):
+        return 2 * index + 1
+
+    def _right_child(self, index):
+        return 2 * index + 2

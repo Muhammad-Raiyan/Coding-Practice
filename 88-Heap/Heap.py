2fcfb4c70afc101dfa7a3ae9b47e2457061b87cc
Raiyan <mislam0525@gmail.com>
2023-10-25 20:29:47 -0400

    Heap insert done

--- 88-Heap/Heap.py
+++ 88-Heap/Heap.py
@@ -15,4 +15,25 @@ class MaxHeap:
         self.heap[index1], self.heap[index2] = self.heap[index2], self.heap[index1]
 
     def insert(self, value):
+        self.heap.append(value)
+        current = len(self.heap) - 1
+
+        while current > 0 and self.heap[current] > self.heap[self._parent(current)]:
+            self._swap(current, self._parent(current))
+            current = self._parent(current)
         pass
+
+
+my_heap = MaxHeap()
+my_heap.insert(99)
+my_heap.insert(72)
+my_heap.insert(61)
+my_heap.insert(58)
+
+print(my_heap.heap)
+
+my_heap.insert(100)
+print(my_heap.heap)
+
+my_heap.insert(75)
+print(my_heap.heap)

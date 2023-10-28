2a02fbf3338030d12ee4b6b1f66694a00e1435bd
Raiyan <mislam0525@gmail.com>
2023-10-28 13:52:27 -0400

    r insert

--- 99-Recursion/rBST.py
+++ 99-Recursion/rBST.py
@@ -10,7 +10,28 @@ class BinarySearchTree:
         self.root = None
 
     def __r_contains(self, current_node, value):
-        pass
+        if current_node == None:
+            return False
+        if value == current_node.value:
+            return True
+        if value < current_node.value:
+            return self.__r_contains(current_node.left, value)
+        else:
+            return self.__r_contains(current_node.right, value)
+
+    def __r_insert(self, current_node, value):
+        if current_node == None:
+            return Node(value)
+        if value < current_node.value:
+            current_node.left = self.__r_insert(current_node.left, value)
+        elif value > current_node.value:
+            current_node.right = self.__r_insert(current_node.right, value)
+        return current_node
 
     def r_contains(self, value):
         return self.__r_contains(self.root, value)
+
+    def r_insert(self, value):
+        if self.root == None:
+            self.root = Node(value)
+        self.__r_insert(self.root, value)

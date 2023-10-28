3a4958995c3654d30d8add06a9a312454fc0c8d2
Raiyan <mislam0525@gmail.com>
2023-10-28 19:06:57 -0400

    r_delete done

--- 99-Recursion/rBST.py
+++ 99-Recursion/rBST.py
@@ -62,14 +62,576 @@ class BinarySearchTree:
             current_node = current_node.left
         return current_node.value
 
+    def __delete_node(self, current_node, value):
+        if current_node is None:
+            return None
+        if value < current_node.value:
+            current_node.left = self.__delete_node(current_node.left, value)
+        elif value > current_node.value:
+            current_node.right = self.__delete_node(current_node.right, value)
+        else:
+            if current_node.left is None and current_node.right is None:
+                return None
+            elif current_node.right is None:
+                return current_node.left
+            elif current_node.left is None:
+                return current_node.right
+            else:
+                current_node.value = self.min_value(current_node.right)
+                self.__delete_node(current_node.right, current_node.value)
 
-"""
-    EXPECTED OUTPUT:
-    ----------------
-	18
-	52
+        return current_node
 
-"""
+    def delete_node(self, value):
+        self.root = self.__delete_node(self.root, value)
+
+
+##########################################################
+##   Test code below will print output to "User logs"   ##
+##########################################################
+
+
+def check(expect, actual, message):
+    print(message)
+    print("EXPECTED:", expect)
+    print("RETURNED:", actual)
+    print("PASS" if expect == actual else "FAIL", "\n")
+
+
+# test_delete_node_no_children
+print("\n----- Test: Delete node with no children -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+bst.delete_node(3)
+check(None, bst.root.left, "Left child of root after deleting 3:")
+
+
+# test_delete_node_only_left_child
+print("\n----- Test: Delete node with only left child -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8, 1]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+bst.delete_node(3)
+check(1, bst.root.left.value, "Left child of root after deleting 3:")
+
+
+# test_delete_node_only_right_child
+print("\n----- Test: Delete node with only right child -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8, 9]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+bst.delete_node(8)
+check(9, bst.root.right.value, "Right child of root after deleting 8:")
+
+
+# test_delete_node_two_children
+print("\n----- Test: Delete node with two children -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8, 1, 4, 7, 9]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+bst.delete_node(3)
+check(4, bst.root.left.value, "Left child of root after deleting 3:")
+
+
+# test_delete_root
+print("\n----- Test: Delete root -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+bst.delete_node(5)
+check(8, bst.root.value, "Root value after deleting 5:")
+
+
+# test_delete_non_existent_node
+print("\n----- Test: Attempt to delete a non-existent node -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+original_root_value = bst.root.value
+bst.delete_node(10)
+check(original_root_value, bst.root.value, "Root value after attempting to delete 10:")
+
+
+##########################################################
+##   Test code below will print output to "User logs"   ##
+##########################################################
+
+
+def check(expect, actual, message):
+    print(message)
+    print("EXPECTED:", expect)
+    print("RETURNED:", actual)
+    print("PASS" if expect == actual else "FAIL", "\n")
+
+
+# test_delete_node_no_children
+print("\n----- Test: Delete node with no children -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+bst.delete_node(3)
+check(None, bst.root.left, "Left child of root after deleting 3:")
+
+
+# test_delete_node_only_left_child
+print("\n----- Test: Delete node with only left child -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8, 1]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+bst.delete_node(3)
+check(1, bst.root.left.value, "Left child of root after deleting 3:")
+
+
+# test_delete_node_only_right_child
+print("\n----- Test: Delete node with only right child -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8, 9]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+bst.delete_node(8)
+check(9, bst.root.right.value, "Right child of root after deleting 8:")
+
+
+# test_delete_node_two_children
+print("\n----- Test: Delete node with two children -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8, 1, 4, 7, 9]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+bst.delete_node(3)
+check(4, bst.root.left.value, "Left child of root after deleting 3:")
+
+
+# test_delete_root
+print("\n----- Test: Delete root -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+bst.delete_node(5)
+check(8, bst.root.value, "Root value after deleting 5:")
+
+
+# test_delete_non_existent_node
+print("\n----- Test: Attempt to delete a non-existent node -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+original_root_value = bst.root.value
+bst.delete_node(10)
+
+check(original_root_value, bst.root.value, "Root value after attempting to delete 10:")
+
+
+##########################################################
+##   Test code below will print output to "User logs"   ##
+##########################################################
+
+
+def check(expect, actual, message):
+    print(message)
+    print("EXPECTED:", expect)
+    print("RETURNED:", actual)
+    print("PASS" if expect == actual else "FAIL", "\n")
+
+
+# test_delete_node_no_children
+print("\n----- Test: Delete node with no children -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+bst.delete_node(3)
+check(None, bst.root.left, "Left child of root after deleting 3:")
+
+
+# test_delete_node_only_left_child
+print("\n----- Test: Delete node with only left child -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8, 1]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+bst.delete_node(3)
+check(1, bst.root.left.value, "Left child of root after deleting 3:")
+
+
+# test_delete_node_only_right_child
+print("\n----- Test: Delete node with only right child -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8, 9]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+bst.delete_node(8)
+check(9, bst.root.right.value, "Right child of root after deleting 8:")
+
+
+# test_delete_node_two_children
+print("\n----- Test: Delete node with two children -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8, 1, 4, 7, 9]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+bst.delete_node(3)
+check(4, bst.root.left.value, "Left child of root after deleting 3:")
+
+
+# test_delete_root
+print("\n----- Test: Delete root -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+bst.delete_node(5)
+check(8, bst.root.value, "Root value after deleting 5:")
+
+
+# test_delete_non_existent_node
+print("\n----- Test: Attempt to delete a non-existent node -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+original_root_value = bst.root.value
+bst.delete_node(10)
+check(original_root_value, bst.root.value, "Root value after attempting to delete 10:")
+
+
+##########################################################
+##   Test code below will print output to "User logs"   ##
+##########################################################
+
+
+def check(expect, actual, message):
+    print(message)
+    print("EXPECTED:", expect)
+    print("RETURNED:", actual)
+    print("PASS" if expect == actual else "FAIL", "\n")
+
+
+# test_delete_node_no_children
+print("\n----- Test: Delete node with no children -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+bst.delete_node(3)
+check(None, bst.root.left, "Left child of root after deleting 3:")
+
+
+# test_delete_node_only_left_child
+print("\n----- Test: Delete node with only left child -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8, 1]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+bst.delete_node(3)
+check(1, bst.root.left.value, "Left child of root after deleting 3:")
+
+
+# test_delete_node_only_right_child
+print("\n----- Test: Delete node with only right child -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8, 9]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+bst.delete_node(8)
+check(9, bst.root.right.value, "Right child of root after deleting 8:")
+
+
+# test_delete_node_two_children
+print("\n----- Test: Delete node with two children -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8, 1, 4, 7, 9]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+bst.delete_node(3)
+check(4, bst.root.left.value, "Left child of root after deleting 3:")
+
+
+# test_delete_root
+print("\n----- Test: Delete root -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+bst.delete_node(5)
+check(8, bst.root.value, "Root value after deleting 5:")
+
+
+# test_delete_non_existent_node
+print("\n----- Test: Attempt to delete a non-existent node -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+original_root_value = bst.root.value
+bst.delete_node(10)
+check(original_root_value, bst.root.value, "Root value after attempting to delete 10:")
+##########################################################
+##   Test code below will print output to "User logs"   ##
+##########################################################
+
+
+def check(expect, actual, message):
+    print(message)
+    print("EXPECTED:", expect)
+    print("RETURNED:", actual)
+    print("PASS" if expect == actual else "FAIL", "\n")
+
+
+# test_delete_node_no_children
+print("\n----- Test: Delete node with no children -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+bst.delete_node(3)
+check(None, bst.root.left, "Left child of root after deleting 3:")
+
+
+# test_delete_node_only_left_child
+print("\n----- Test: Delete node with only left child -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8, 1]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+bst.delete_node(3)
+check(1, bst.root.left.value, "Left child of root after deleting 3:")
+
+
+# test_delete_node_only_right_child
+print("\n----- Test: Delete node with only right child -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8, 9]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+bst.delete_node(8)
+check(9, bst.root.right.value, "Right child of root after deleting 8:")
+
+
+# test_delete_node_two_children
+print("\n----- Test: Delete node with two children -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8, 1, 4, 7, 9]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+bst.delete_node(3)
+check(4, bst.root.left.value, "Left child of root after deleting 3:")
+
+
+# test_delete_root
+print("\n----- Test: Delete root -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+bst.delete_node(5)
+check(8, bst.root.value, "Root value after deleting 5:")
+
+
+# test_delete_non_existent_node
+print("\n----- Test: Attempt to delete a non-existent node -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+original_root_value = bst.root.value
+bst.delete_node(10)
+check(original_root_value, bst.root.value, "Root value after attempting to delete 10:")
+##########################################################
+##   Test code below will print output to "User logs"   ##
+##########################################################
+
+
+def check(expect, actual, message):
+    print(message)
+    print("EXPECTED:", expect)
+    print("RETURNED:", actual)
+    print("PASS" if expect == actual else "FAIL", "\n")
+
+
+# test_delete_node_no_children
+print("\n----- Test: Delete node with no children -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+bst.delete_node(3)
+check(None, bst.root.left, "Left child of root after deleting 3:")
+
+
+# test_delete_node_only_left_child
+print("\n----- Test: Delete node with only left child -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8, 1]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+bst.delete_node(3)
+check(1, bst.root.left.value, "Left child of root after deleting 3:")
+
+
+# test_delete_node_only_right_child
+print("\n----- Test: Delete node with only right child -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8, 9]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+bst.delete_node(8)
+check(9, bst.root.right.value, "Right child of root after deleting 8:")
+
+
+# test_delete_node_two_children
+print("\n----- Test: Delete node with two children -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8, 1, 4, 7, 9]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+bst.delete_node(3)
+check(4, bst.root.left.value, "Left child of root after deleting 3:")
+
+
+# test_delete_root
+print("\n----- Test: Delete root -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+bst.delete_node(5)
+check(8, bst.root.value, "Root value after deleting 5:")
+
+
+# test_delete_non_existent_node
+print("\n----- Test: Attempt to delete a non-existent node -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+original_root_value = bst.root.value
+bst.delete_node(10)
+check(original_root_value, bst.root.value, "Root value after attempting to delete 10:")
+##########################################################
+##   Test code below will print output to "User logs"   ##
+##########################################################
+
+
+def check(expect, actual, message):
+    print(message)
+    print("EXPECTED:", expect)
+    print("RETURNED:", actual)
+    print("PASS" if expect == actual else "FAIL", "\n")
+
+
+# test_delete_node_no_children
+print("\n----- Test: Delete node with no children -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+bst.delete_node(3)
+check(None, bst.root.left, "Left child of root after deleting 3:")
+
+
+# test_delete_node_only_left_child
+print("\n----- Test: Delete node with only left child -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8, 1]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+bst.delete_node(3)
+check(1, bst.root.left.value, "Left child of root after deleting 3:")
+
+
+# test_delete_node_only_right_child
+print("\n----- Test: Delete node with only right child -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8, 9]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+bst.delete_node(8)
+check(9, bst.root.right.value, "Right child of root after deleting 8:")
+
+
+# test_delete_node_two_children
+print("\n----- Test: Delete node with two children -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8, 1, 4, 7, 9]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+bst.delete_node(3)
+check(4, bst.root.left.value, "Left child of root after deleting 3:")
+
+
+# test_delete_root
+print("\n----- Test: Delete root -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+bst.delete_node(5)
+check(8, bst.root.value, "Root value after deleting 5:")
+
+
+# test_delete_non_existent_node
+print("\n----- Test: Attempt to delete a non-existent node -----\n")
+bst = BinarySearchTree()
+values = [5, 3, 8]
+for v in values:
+    print("Inserting value:", v)
+    bst.r_insert(v)
+original_root_value = bst.root.value
+bst.delete_node(10)
+check(original_root_value, bst.root.value, "Root value after attempting to delete 10:")
 
 my_tree = BinarySearchTree()
 my_tree.insert(47)

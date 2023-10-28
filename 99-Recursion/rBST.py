a6d0594e4a9cc3446b8b6a4a58d2447dbdaacdb7
Raiyan <mislam0525@gmail.com>
2023-10-28 17:08:11 -0400

    Implemented r_insert, r_contains, min_value

--- 99-Recursion/rBST.py
+++ 99-Recursion/rBST.py
@@ -1,37 +1,169 @@
 class Node:
-    def __init__(self, value) -> None:
+    def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
 
 
 class BinarySearchTree:
-    def __init__(self) -> None:
+    def __init__(self):
         self.root = None
+
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
+            return True
+        temp = self.root
+        while True:
+            if new_node.value == temp.value:
+                return False
+            if new_node.value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+            else:
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
 
     def __r_contains(self, current_node, value):
         if current_node == None:
             return False
-        if value == current_node.value:
+        if current_node.value == value:
             return True
         if value < current_node.value:
             return self.__r_contains(current_node.left, value)
-        else:
+        if value > current_node.value:
             return self.__r_contains(current_node.right, value)
 
     def __r_insert(self, current_node, value):
         if current_node == None:
-            return Node(value)
+            current_node = Node(value)
         if value < current_node.value:
             current_node.left = self.__r_insert(current_node.left, value)
-        elif value > current_node.value:
+        if value > current_node.value:
             current_node.right = self.__r_insert(current_node.right, value)
         return current_node
-
-    def r_contains(self, value):
-        return self.__r_contains(self.root, value)
 
     def r_insert(self, value):
         if self.root == None:
             self.root = Node(value)
-        self.__r_insert(self.root, value)
+            return
+        self.root = self.__r_insert(self.root, value)
+
+    def r_contains(self, value):
+        return self.__r_contains(self.root, value)
+
+    def min_value(self, current_node):
+        while current_node.left is not None:
+            current_node = current_node.left
+        return current_node.value
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+	18
+	52
+
+"""
+
+my_tree = BinarySearchTree()
+my_tree.insert(47)
+my_tree.insert(21)
+my_tree.insert(76)
+my_tree.insert(18)
+my_tree.insert(27)
+my_tree.insert(52)
+my_tree.insert(82)
+
+print("BST Contains 27:")
+print(my_tree.r_contains(27))
+
+print("\nBST Contains 17:")
+print(my_tree.r_contains(17))
+
+print(my_tree.min_value(my_tree.root))
+
+print(my_tree.min_value(my_tree.root.right))
+
+##########################################################
+##   Test code below will print output to "User logs"   ##
+##########################################################
+
+
+def check(expect, actual, message):
+    print(message)
+    print("EXPECTED:", expect)
+    print("RETURNED:", actual)
+    print("PASS" if expect == actual else "FAIL", "\n")
+
+
+print("\n----- Test: Insert into an empty tree -----\n")
+bst = BinarySearchTree()
+print("Inserting value:", 5)
+bst.r_insert(5)
+check(5, bst.root.value, "Root value after inserting 5:")
+check(None, bst.root.left, "Left child of root:")
+check(None, bst.root.right, "Right child of root:")
+
+print("\n----- Test: Insert values in ascending order -----\n")
+bst = BinarySearchTree()
+values = [1, 2, 3, 4, 5]
+for val in values:
+    print("Inserting value:", val)
+    bst.r_insert(val)
+
+# Check tree structure
+check(1, bst.root.value, "Root value:")
+check(2, bst.root.right.value, "Right child of root:")
+check(3, bst.root.right.right.value, "Right child of right child of root:")
+check(
+    4,
+    bst.root.right.right.right.value,
+    "Right child's right child's right child of root:",
+)
+check(5, bst.root.right.right.right.right.value, "Fourth right child of root:")
+
+print("\n----- Test: Insert values in descending order -----\n")
+bst = BinarySearchTree()
+values = [5, 4, 3, 2, 1]
+for val in values:
+    print("Inserting value:", val)
+    bst.r_insert(val)
+
+# Check tree structure
+check(5, bst.root.value, "Root value:")
+check(4, bst.root.left.value, "Left child of root:")
+check(3, bst.root.left.left.value, "Left child of left child of root:")
+check(2, bst.root.left.left.left.value, "Left child's left child's left child of root:")
+check(1, bst.root.left.left.left.left.value, "Fourth left child of root:")
+
+print("\n----- Test: Insert values in mixed order -----\n")
+bst = BinarySearchTree()
+values = [3, 1, 4, 5, 2]
+for val in values:
+    print("Inserting value:", val)
+    bst.r_insert(val)
+
+# Check tree structure
+check(3, bst.root.value, "Root value:")
+check(1, bst.root.left.value, "Left child of root:")
+check(2, bst.root.left.right.value, "Right child of left child of root:")
+check(4, bst.root.right.value, "Right child of root:")
+check(5, bst.root.right.right.value, "Right child of right child of root:")
+
+print("\n----- Test: Insert duplicate values -----\n")
+bst = BinarySearchTree()
+values = [3, 3, 3]
+for val in values:
+    print("Inserting value:", val)
+    bst.r_insert(val)
+
+# Check tree structure
+check(3, bst.root.value, "Root value:")
+check(None, bst.root.left, "Left child of root:")
+check(None, bst.root.right, "Right child of root:")
